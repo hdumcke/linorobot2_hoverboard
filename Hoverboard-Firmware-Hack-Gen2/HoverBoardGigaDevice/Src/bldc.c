9df66b915c2a813a482e1d9cf7d5f9f8013c3e36
Horst Dumcke <hdumcke@marlysys.com>
2023-02-27 08:05:31 +0100

    working on PID controller

--- Hoverboard-Firmware-Hack-Gen2/HoverBoardGigaDevice/Src/bldc.c
+++ Hoverboard-Firmware-Hack-Gen2/HoverBoardGigaDevice/Src/bldc.c
@@ -254,14 +254,14 @@ void CalculateBLDC(void)
 		}
 	}
 	// PID controller frequency 100 Hz
-	// realSpeed in rpm, use conversion factor
+	// convert realSpeed to rpm
         if (buzzerTimer % 160 == 0)
 	{
 		#ifdef MASTER
-		SetPWM(updatePID((float)speedM, realSpeed / 20., 0.01));
+		SetPWM(updatePID((float)speedM, realSpeed / 100., 0.01));
 		#endif
 		#ifdef SLAVE
-		SetPWM(updatePID((float)desiredSpeedSlave, realSpeed / 20., 0.01));
+		SetPWM(updatePID((float)desiredSpeedSlave, realSpeed / 100., 0.01));
 		#endif
 	}
 	// Calculate low-pass filter for pwm value
